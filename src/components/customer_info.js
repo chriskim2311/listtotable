c993ecf55fbf8e09b3368b41885bbac21b342f07
Oleksandr Dykun <sashaluckyman@gmail.com>
2018-11-24 16:59:11 -0800

    create customer info component

---
+++ src/components/customer_info.js
@@ -0,0 +1,137 @@
+import React, { Component } from 'react';
+
+
+class CustomerInfo extends Component {
+    render(){
+        return(
+            <div>
+                <div className="row blue">
+                    <div className="col s1">
+                        <p>1</p>
+                    </div>
+                    <div className="col s4">
+                        <ul>
+                            <li>Name</li>
+                            <li>party of 5</li>
+                            <li>773-777-7777</li>
+                        </ul>
+                    </div>
+                    <div className="col s2 ">
+                        <p>
+                            <button>notify</button>
+                        </p>
+                    </div>
+                    <div className="col s2">
+                        <p>
+                            <button>seat</button>
+                        </p>
+                    </div>
+                    <div className="col s1">
+                        <p>del</p>
+                    </div>
+                </div>
+                <div className="row blue">
+                    <div className="col s1">
+                        <p>2</p>
+                    </div>
+                    <div className="col s4">
+                        <ul>
+                            <li>Name</li>
+                            <li>party of 5</li>
+                            <li>773-777-7777</li>
+                        </ul>
+                    </div>
+                    <div className="col s2 ">
+                        <p>
+                            <button>notify</button>
+                        </p>
+                    </div>
+                    <div className="col s2">
+                        <p>
+                            <button>seat</button>
+                        </p>
+                    </div>
+                    <div className="col s1">
+                        <p>del</p>
+                    </div>
+                </div>
+                <div className="row blue">
+                    <div className="col s1">
+                        <p>3</p>
+                    </div>
+                    <div className="col s4">
+                        <ul>
+                            <li>Name</li>
+                            <li>party of 5</li>
+                            <li>773-777-7777</li>
+                        </ul>
+                    </div>
+                    <div className="col s2 ">
+                        <p>
+                            <button>notify</button>
+                        </p>
+                    </div>
+                    <div className="col s2">
+                        <p>
+                            <button>seat</button>
+                        </p>
+                    </div>
+                    <div className="col s1">
+                        <p>del</p>
+                    </div>
+                </div>
+                <div className="row blue">
+                    <div className="col s1">
+                        <p>4</p>
+                    </div>
+                    <div className="col s4">
+                        <ul>
+                            <li>Name</li>
+                            <li>party of 5</li>
+                            <li>773-777-7777</li>
+                        </ul>
+                    </div>
+                    <div className="col s2 ">
+                        <p>
+                            <button>notify</button>
+                        </p>
+                    </div>
+                    <div className="col s2">
+                        <p>
+                            <button>seat</button>
+                        </p>
+                    </div>
+                    <div className="col s1">
+                        <p>del</p>
+                    </div>
+                </div>
+                <div className="row blue">
+                    <div className="col s1">
+                        <p>5</p>
+                    </div>
+                    <div className="col s4">
+                        <ul>
+                            <li>Name</li>
+                            <li>party of 5</li>
+                            <li>773-777-7777</li>
+                        </ul>
+                    </div>
+                    <div className="col s2 ">
+                        <p>
+                            <button>notify</button>
+                        </p>
+                    </div>
+                    <div className="col s2">
+                        <p>
+                            <button>seat</button>
+                        </p>
+                    </div>
+                    <div className="col s1">
+                        <p>del</p>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+export default CustomerInfo;

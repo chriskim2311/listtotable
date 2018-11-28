284776f4475403d17b398fed48dcb86a701ef04a
mchu79 <mixchu79@gmail.com>
2018-11-27 18:40:24 -0800

    getAll endpoints

---
+++ public/api/clients/getAll.php
@@ -0,0 +1,16 @@
+<?php
+
+$query = "SELECT * FROM Clients";
+
+$result = mysqli_query($conn, $query);
+
+if (mysqli_num_rows($result)>0) {
+    while($row = mysqli_fetch_assoc($result)) {
+        $output['clients'][] = $row;
+    }
+    $output['success'] = true;
+} else {
+    $output['message'] = 'No users found';
+}
+
+?>
--- public/api/tablefinder.php
+++ public/api/tablefinder.php
@@ -12,6 +12,18 @@ switch($action) {
     case 'restaurants':
         require_once('restaurants/getAll.php');
         break;
+
+    case 'clients':
+        require_once('clients/getAll.php');
+        break;
+
+    case 'restaurant_users':
+        require_once('restaurant_users/getAll.php');
+        break;
+
+    case 'parties':
+        require_once('parties/getAll.php');
+        break;
 }
 
 $output = json_encode($output);

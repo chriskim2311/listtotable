03d0be22c829a7caca94704360584edba9a4bae9
Oleksandr Dykun <sashaluckyman@gmail.com>
2018-11-27 21:46:49 -0800

    change login form to use redux

--- src/components/r_check-in.js
+++ src/components/r_check-in.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import RTopMenu from './r_arrowAndMenu';
 import ListMenu from './list_menu';
-import ReservationInfo from './reservationInfo';
+import CheckInForm from './check-in-form';
 class RCheckIn extends Component {
     render(){
         return(
             <div>
                 <RTopMenu/>
                 <ListMenu/>
-                <ReservationInfo/>
+                <CheckInForm/>
             </div>
             
         )

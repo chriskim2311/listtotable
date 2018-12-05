699f19d5a343f3efd53a03d3f396fb3750ab2121
mchu79 <mixchu79@gmail.com>
2018-12-04 18:40:06 -0800

    changed syntax

--- public/api/restaurants/index.php
+++ public/api/restaurants/index.php
@@ -4,13 +4,13 @@ $method = $_GET['method'];
 
 switch($method) {
     case 'getAll':
-        require_once('./getAll.php');
+        require_once('getAll.php');
         break;
     case 'insert':
-        require_once('./insert.php');
+        require_once('insert.php');
         break;
     case 'delete':
-        require_once('./delete.php');
+        require_once('delete.php');
         break;
 }
 

05edfd6603f0a795ff198fa082c7215025bf2e48
mchu79 <mixchu79@gmail.com>
2018-12-04 18:43:05 -0800

    syntax changes for restaurant users

--- public/api/restaurant_users/index.php
+++ public/api/restaurant_users/index.php
@@ -4,22 +4,22 @@ $method = $_GET['method'];
 
 switch($method) {
     case 'getAll':
-        require_once('./getAll.php');
+        require_once('getAll.php');
         break;
 
     case 'insert':
-        require_once('./insert.php');
+        require_once('insert.php');
         break;
 
     case 'delete':
-        require_once('./delete.php');
+        require_once('delete.php');
         break;
 
     case 'login':
-        require_once('./login.php');
+        require_once('login.php');
         break;
 
     case 'logout':
-        require_once('./logout.php');
+        require_once('logout.php');
         break;
 }

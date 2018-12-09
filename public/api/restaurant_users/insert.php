bc8ff525f89d631bff719f6da56b3e15d5a436aa
Oleksandr Dykun <sashaluckyman@gmail.com>
2018-12-08 17:30:28 -0800

    login as yardhouse and asdf working

--- public/api/restaurant_users/insert.php
+++ public/api/restaurant_users/insert.php
@@ -1,14 +1,16 @@
 <?php
+
+$postdata = file_get_contents('php://input');
+$_POST = json_decode($postdata, true);
+
 $restaurant_ID = $_POST['restaurant_ID'];
 $username = $_POST['username'];
 $email = $_POST['email'];
 $password = sha1($_POST['password']);
 $status = $_POST['status'];
-$last_accessed = $_POST['last_accessed'];
-$created = $_POST['created'];
 $comments = $_POST['comments'];
 
-$query = "INSERT INTO restaurant_users (restaurant_ID, username, email, password, status, last_accessed, created, comments) VALUES ('$restaurant_ID', '$username', '$email', '$password', '$status', '$last_accessed','$created', '$comments')";
+$query = "INSERT INTO restaurant_users (restaurant_ID, username, email, password, status, last_accessed, created, comments) VALUES ('$restaurant_ID', '$username', '$email', '$password', '$status', NULL,NULL, '$comments')";
 
 if (mysqli_query($conn, $query)){
     $output['success'] = true;

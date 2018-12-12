173695443322bfd902efa5c85f3a9e7c95854c5e
chriskim2311 <chriskim2311@gmail.com>
2018-12-11 16:58:35 -0800

    fixed bugs on customer info

--- src/components/customer_info.js
+++ src/components/customer_info.js
@@ -9,14 +9,14 @@ import 'materialize-css/dist/js/materialize';
 
 
 class CustomerInfo extends Component {
-    componentDidMount(){
+    componentDidMount() {
         const waitingObj = {
-            restaurant_id:'ChIJleVgXPnn3IARUGDd-mGJHYw',
+            restaurant_id: 'ChIJleVgXPnn3IARUGDd-mGJHYw',
             status: 1
         }
 
         const notifiedObj = {
-            restaurant_id:'ChIJleVgXPnn3IARUGDd-mGJHYw',
+            restaurant_id: 'ChIJleVgXPnn3IARUGDd-mGJHYw',
             status: 2
         }
         // console.log('data after componentdid mount', this.props)
@@ -28,28 +28,28 @@ class CustomerInfo extends Component {
     //     this.props.waitingListData();
     // }
 
-    handleNotify(){
+    handleNotify() {
 
         // console.log("PHONEEEE", key)
         // const phoneNumber = this.props.waiting_list[0].phoneNumber;
         axios.post('/api/tablefinder.php?action=clients&method=updateNotified',
             phone
-        
+
         );
-        
+
         // console.log(' UPDATE Notified call response:', resp);
         // const restaurantID 
         // this.props.notifiedListData(phone);
         // const restaurantName = null;
-        axios.post('http://place.kim-chris.com/message/notify',{
+        axios.post('http://place.kim-chris.com/message/notify', {
             restaurant: "RESTAURANT_NAME",
-            phone_number: phone 
+            phone_number: phone
         }).then(resp => {
             // console.log("CHECKED INNNN:", resp)
         })
     }
 
-    convertTime(inputString){
+    convertTime(inputString) {
         //GET NEW DATE AND TIME IN MILLISECONDS
         var newDate = new Date();
         var newTime = newDate.getTime();
@@ -69,34 +69,31 @@ class CustomerInfo extends Component {
         return minutes;
     }
 
-    renderNotifiedListOnDom(){
+    renderNotifiedListOnDom() {
         // console.log('+++++++++ props:', this.props)
         const notified = this.props.notified_list;
 
         // console.log('partys from server on customerinfo page',notified)
 
-        if(!notified){
+        if (!notified) {
             return
         }
 
         const notifiedList = notified.map((current, index) => {
             const name = current.client_name;
             const partyOf = current.table_size
-
             const phone = current.phone_number;
             const ID = current.ID;
-
-            const phone = current.phone_number
             const timeWhenNotified = current.wait_notify;
             const waitTimeSinceNotified = this.convertTime(timeWhenNotified);
             console.log("TIME SINCE NOTIFIED:", waitTimeSinceNotified);
 
-        
-            return(
+
+            return (
                 <div key={index}>
                     <div className="row yellow">
                         <div className="col s1">
-                            <p>{index +1}</p>
+                            <p>{index + 1}</p>
                         </div>
                         <div className="col s4">
                             <ul>
@@ -108,109 +105,109 @@ class CustomerInfo extends Component {
                         <div className="col s2 ">
                             <p>
                                 <button
-                                 onClick={this.handleNotifyssss}>notify</button>
+                                    onClick={this.handleNotifyssss}>notify</button>
                             </p>
-                            
+
                         </div>
                         <div className="col s2">
                             <p>
-                                <button onClick={()=>this.props.updatedSeated(ID)} >seat</button>
+                                <button onClick={() => this.props.updatedSeated(ID)} >seat</button>
                             </p>
                         </div>
-                        <div className="col s1" onClick={()=> this.props.deleteListItem(phone)}>
+                        <div className="col s1" onClick={() => this.props.deleteListItem(phone)}>
                             <p>del</p>
                         </div>
-                        div className="">
-                            <div className="col s12">
-                                Notified {waitTimeSinceNotified} minutes ago
+
+                        <div className="col s12">
+                            Notified {waitTimeSinceNotified} minutes ago
                             </div>
-                        </div>
                     </div>
                 </div>
+              
             )
-        })
+    })
         return notifiedList;
     }
-    renderCustomerListOnDom(){
-       
+renderCustomerListOnDom(){
 
-        const partys = this.props.waiting_list;
-        // console.log('partys on customerinfo page',partys)
 
-        if(!partys){
-            return
-        }
+    const partys = this.props.waiting_list;
+    // console.log('partys on customerinfo page',partys)
 
-        const customerList = partys.map((current, index) => {
-            const name = current.client_name;
-            const partyOf = current.table_size
-            const phone = current.phone_number
-            const restaurantName = current.restaurant_name;
-            const ID = current.ID;
-            const timeWhenAdded = current.wait_start;
-            const waitTimeSinceAdded = this.convertTime(timeWhenAdded);
-            console.log("TIME SINCE ADDED:", waitTimeSinceAdded);
+    if (!partys) {
+        return
+    }
 
-        
-            return(
-                <div key={index}>
-                    <div className="row blue">
-                        <div className="col s1">
-                            <p>{index +1}</p>
-                        </div>
-                        <div className="col s4">
-                            <ul>
-                                <li>{name}</li>
-                                <li>{partyOf}</li>
-                                <li>{phone}</li>
-                            </ul>
-                        </div>
-                        <div className="col s2 ">
-                            <p>
-                                <button
-                                 onClick={()=>this.props.updateNotified(restaurantName, ID, phone)}>notify</button>
-                            </p>
-                            
-                        </div>
-                        <div className="col s2">
-                            <p>
-                                <button onClick={()=>this.props.updatedSeated(ID)}  >seat</button>
-                            </p>
-                        </div>
-                        <div className="col s1" onClick={()=> this.props.deleteListItem(phone)}>
-                            <p>del</p>
-                        </div>
-                        <div className="">
-                            <div className="col s12">
-                                Added {waitTimeSinceAdded} minutes ago
+    const customerList = partys.map((current, index) => {
+        const name = current.client_name;
+        const partyOf = current.table_size
+        const phone = current.phone_number
+        const restaurantName = current.restaurant_name;
+        const ID = current.ID;
+        const timeWhenAdded = current.wait_start;
+        const waitTimeSinceAdded = this.convertTime(timeWhenAdded);
+        console.log("TIME SINCE ADDED:", waitTimeSinceAdded);
+
+
+        return (
+            <div key={index}>
+                <div className="row blue">
+                    <div className="col s1">
+                        <p>{index + 1}</p>
+                    </div>
+                    <div className="col s4">
+                        <ul>
+                            <li>{name}</li>
+                            <li>{partyOf}</li>
+                            <li>{phone}</li>
+                        </ul>
+                    </div>
+                    <div className="col s2 ">
+                        <p>
+                            <button
+                                onClick={() => this.props.updateNotified(restaurantName, ID, phone)}>notify</button>
+                        </p>
+
+                    </div>
+                    <div className="col s2">
+                        <p>
+                            <button onClick={() => this.props.updatedSeated(ID)}  >seat</button>
+                        </p>
+                    </div>
+                    <div className="col s1" onClick={() => this.props.deleteListItem(phone)}>
+                        <p>del</p>
+                    </div>
+                    <div className="">
+                        <div className="col s12">
+                            Added {waitTimeSinceAdded} minutes ago
                             </div>
-                        </div>
                     </div>
                 </div>
-            )
-        })
-        return customerList;
-    
-        }
-    
-    render(){
-        
-        return(
-            <Fragment>
-                <div>
-                    {this.renderNotifiedListOnDom()}
-                </div>
-                <div>
-                    {this.renderCustomerListOnDom()}
-                </div>
-                
-            </Fragment>
+            </div>
         )
-    }
+    })
+    return customerList;
+
+}
+
+render(){
+
+    return (
+        <Fragment>
+            <div>
+                {this.renderNotifiedListOnDom()}
+            </div>
+            <div>
+                {this.renderCustomerListOnDom()}
+            </div>
+
+        </Fragment>
+    )
+}
 }
 
 
-function mapStateToProps(state){
+function mapStateToProps(state) {
     // console.log('Redux State:', state);
 
     return {
@@ -219,12 +216,12 @@ function mapStateToProps(state){
     }
 }
 
-export default connect(mapStateToProps,{
+export default connect(mapStateToProps, {
     deleteListItem: deleteListItem,
     waitingListData: getWaitingListData,
     notifiedListData: getNotifiedListData,
     updateNotified: changeNotifyStatus,
     updatedSeated: changeSeatedStatus
-    
+
 
 })(CustomerInfo);

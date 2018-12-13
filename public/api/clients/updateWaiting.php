c91980e58cffe7afba4627bf40dc307289dc6885
mchu79 <mixchu79@gmail.com>
2018-12-12 16:09:23 -0800

    return restaurant id and timestamp notified seated

--- public/api/clients/updateWaiting.php
+++ public/api/clients/updateWaiting.php
@@ -7,9 +7,10 @@ $_POST = json_decode($postdata, true);
 
 $ID = ($_POST['ID']);
 $status = $_POST['status'];
+$wait_notify = $_POST['wait_notify'];
 
 
-$query = "UPDATE clients SET status = 2 WHERE ID = '$ID'";
+$query = "UPDATE clients SET status = 2, wait_notify = NOW() WHERE ID = '$ID'";
 
 if (mysqli_query($conn, $query)){
     $output['success'] = true;

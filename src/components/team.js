987d129e4b0c42a2ebd8797b72aadffc4bf6c85e
apark105 <andypark105@gmail.com>
2018-12-13 19:36:53 -0800

    Added Github and Linkedin Info

--- src/components/team.js
+++ src/components/team.js
@@ -2,7 +2,7 @@
 import React, { Component } from 'react';
 import Navigation from './hamburgerAndBack';
 import '../assets/css/teamPage.css';
-import gmail from '../assets/images/socialMedia/gmail.png';
+import portfolio from '../assets/images/socialMedia/portfolio.png';
 import github from '../assets/images/socialMedia/github.png'; 
 import linkedIn from '../assets/images/socialMedia/linkedIn.png';
 
@@ -15,16 +15,16 @@ export default class Team extends Component {
                 Name: 'Chris Kim',
                 Title: 'Project Lead',
                 LinkedIn: 'https://www.linkedin.com/in/christopher-kim-71898949/',
-                Github: '',
-                Email: '',
+                Github: 'https://github.com/chriskim2311',
+                Portfolio: 'kimchris.dev@gmail.com',
                 Photo: '',
             },
             {
                 Name: 'Andy Park',
                 Title: 'Front-End Developer',
                 LinkedIn: 'https://www.linkedin.com/in/andypark105/',
-                Github: '',
-                Email: '',
+                Github: 'https://github.com/apark105',
+                Portfolio: 'andypark105@gmail.com',
                 Photo: '',
             },
             {
@@ -32,7 +32,7 @@ export default class Team extends Component {
                 Title: 'Front-End Developer',
                 LinkedIn: 'https://www.linkedin.com/in/sasha-oleksandr-dykun-442b04171/',
                 Github: '',
-                Email: '',
+                Portfolio: '',
                 Photo: '',
             },
             {
@@ -40,15 +40,15 @@ export default class Team extends Component {
                 Title: 'Front-End Developer',
                 LinkedIn: 'https://www.linkedin.com/in/andrew-ouatu-776774b4/',
                 Github: '',
-                Email: '',
+                Portfolio: '',
                 Photo: '',
             },
             {
                 Name: 'Michael Chu',
                 Title: 'Back-End Developer',
                 LinkedIn: 'https://www.linkedin.com/in/michaeljchu/',
-                Github: '',
-                Email: '',
+                Github: 'https://github.com/mchu79',
+                Portfolio: 'michaelchu1979@gmail.com',
                 Photo: '',
             },
             {
@@ -56,13 +56,13 @@ export default class Team extends Component {
                 Title: 'Project Manager',
                 LinkedIn: 'https://www.linkedin.com/in/andy-ong/',
                 Github: '',
-                Email: '',
+                Portfolio: '',
                 Photo: '',
             },
         ]
         const individualRow = memberInfos.map((memberInfo) => {
-            debugger
-            const { Name, Title, LinkedIn, Github, Email } = memberInfo
+            
+            const { Name, Title, LinkedIn, Github, Portfolio } = memberInfo
             return (
                 <div className="individualRow">
                     <div className="photoIcon">
@@ -72,9 +72,9 @@ export default class Team extends Component {
                         <div className="individualName">{Name}</div>
                         <div className="individualTitle">{Title}</div>
                         <div className="socialMediaIconContainer">
-                            <div className="socialMediaIcon"><img src={github}/></div>
-                            <div className="socialMediaIcon"><img src={linkedIn}/></div>
-                            <div className="socialMediaIcon"><img src={gmail}/></div>
+                            <div className="socialMediaIcon"><a href={Github}><img className="smIcon" src={github}/></a></div>
+                            <div className="socialMediaIcon"><a href={LinkedIn}><img className="smIcon" src={linkedIn}/></a></div>
+                            <div className="socialMediaIcon"><a href={Portfolio}><img className="smIcon" src={portfolio}/></a></div>
                         </div>
                     </div>
                 </div>

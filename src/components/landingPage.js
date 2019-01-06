dcd3552f97db398ca3798ce4037308181e404248
apark105 <andypark105@gmail.com>
2019-01-05 22:26:22 -0800

    added media query to landing page

--- src/components/landingPage.js
+++ src/components/landingPage.js
@@ -6,6 +6,7 @@ import '../assets/css/landingPage.css';
 import logo from '../assets/images/list-to-table-logo-white.png';
 import { Link } from 'react-router-dom';
 import { checkCurrentPosition, setCurrentPosition } from '../actions';
+import Media from 'react-media';
 
 class LandingPage extends Component{
     constructor(props) {
@@ -51,30 +52,68 @@ class LandingPage extends Component{
         console.warn(set)
         return (
             <Fragment> 
+                
                 <div className="landingPageContainer">
-                    <div className='logoContainer'>
-                        <img src={logo}/>
-                    </div>
-                    <div className='titleContainer'>
-                        <div className="title">List To Table</div>
-                    </div>
-                    <div className="buttonsContainer">
-                        <div className='buttonsBox'>
-                            <button id="restaurants-button" className="btn btn-large waves-effect waves-light">
-                                <Link className ="restaurants" to="/login">restaurants</Link>
-                            </button>
-                            <button id="guests-button" className="btn btn-large waves-effect waves-light">
-                                <div className={ loadSpinner ? "hideSpinner" : "spinner"}>
-                                    <div className={ loadSpinner ? "hideSpinner" : "bounce1"}></div>
-                                    <div className={ loadSpinner ? "hideSpinner" : "bounce2"}></div>
-                                    <div className={ loadSpinner ? "hideSpinner" : "bounce3"}></div>
-                                </div>
-                                <Link className ="guests" to={set ? "/customer-map" : "/custom-location"}>guests</Link>
-                            </button>
-                        </div>
-                    </div>
-                    <div className="bottomSpacer">
-                    </div>
+                    <Media query={{ minWidth: 545 }}>
+                        {matches => 
+                            matches ? (
+                                <Fragment>
+                                    <div className="headContainer">
+                                        <div className="divLogo">
+                                            <img src={logo} />
+                                        </div>
+                                        <nav className="navigationHeader">
+                                            <div className="nav-wrapper">
+                                                <ul className="navHead right">
+                                                    <li>
+                                                        <Link to="/">Home</Link>
+                                                    </li>
+                                                    <li>
+                                                        <Link to="/customer-map">Map</Link>
+                                                    </li>
+                                                    <li>
+                                                        <Link to="/about">About</Link>
+                                                    </li>
+                                                    <li>
+                                                        <Link to="/team">Team</Link>
+                                                    </li>
+                                                </ul>
+                                            </div>
+                                        </nav>
+                                        <div className="titleLogo"></div>    
+                                    </div>
+                                    <div className="detailContainer"></div>
+                                </Fragment>
+                            ) : (
+                                <Fragment>
+                                    <div className='logoContainer'>
+                                        <img src={logo}/>
+                                    </div>
+                                    <div className='titleContainer'>
+                                        <div className="title">List To Table</div>
+                                    </div>
+                                    <div className="buttonsContainer">
+                                        <div className='buttonsBox'>
+                                            <button id="restaurants-button" className="btn btn-large waves-effect waves-light">
+                                                <Link className ="restaurants" to="/login">restaurants</Link>
+                                            </button>
+                                            <button id="guests-button" className="btn btn-large waves-effect waves-light">
+                                                <div className={ loadSpinner ? "hideSpinner" : "spinner"}>
+                                                    <div className={ loadSpinner ? "hideSpinner" : "bounce1"}></div>
+                                                    <div className={ loadSpinner ? "hideSpinner" : "bounce2"}></div>
+                                                    <div className={ loadSpinner ? "hideSpinner" : "bounce3"}></div>
+                                                </div>
+                                                <Link className ="guests" to={set ? "/customer-map" : "/custom-location"}>guests</Link>
+                                            </button>
+                                        </div>
+                                    </div>
+                                    <div className="bottomSpacer">
+                                    </div>
+                                </Fragment>
+                            )
+                        }
+                    </Media>
+                    
                 </div>
             </Fragment>
         )

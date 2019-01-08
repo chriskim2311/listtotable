b11d06bda3134dccc083a923e98ed75efa9a06d8
Oleksandr Dykun <sashaluckyman@gmail.com>
2019-01-07 23:25:53 -0800

    create urlapi config file and add it to gitignore

---
+++ src/config/index.js
@@ -0,0 +1,5 @@
+import urlConfig from './url';
+
+export const busyHoursUrl = urlConfig.BUSY_HOURS_URL;
+export const messageComfirmUrl = urlConfig.MESSAGE_CONFIRM_URL;
+export const messageNotifyUrl = urlConfig.MESSAGE_NOTIFY_URL;

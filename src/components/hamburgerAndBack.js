0075ca7e9dd2695ec1ecb0c65e9649c42972bf00
apark105 <andyjpark105@gmail.com>
2019-01-15 00:49:30 -0800

    fixed a lot of minor css layouts

--- src/components/hamburgerAndBack.js
+++ src/components/hamburgerAndBack.js
@@ -43,12 +43,15 @@ class Navigation extends Component {
                         arrow_back
                         </i>
 
-                    {this.props.location.pathname === "/" ? null : (<NavLink to="/">
-                        <div className="small-nav-title">
-                            <img src={logo} />
-                        </div>
-                        <h1 className="large-nav-title">List To Table</h1>
-                    </NavLink>)}
+                    {this.props.location.pathname === "/" ? null : (
+                        <Fragment>
+                            <div className="small-nav-title">
+                                <img src={logo} />
+                            </div>
+                            <h1 className="large-nav-title">List To Table</h1>
+                        </Fragment>
+                        
+                   )}
 
 
                     <div
